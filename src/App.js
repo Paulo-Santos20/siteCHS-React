4061d4c5c1ee9d4b5b85ed458ce679e4a8d541f9
Paulo-Santos20 <paulo_santos20@outlook.com.br>
2023-09-03 23:21:35 -0300

    feat: slide

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { Navbar } from "./components/Navbar";
-import { Home } from "./components/Home"
+import SlideShow, { Home } from "./components/Home"
 
 function App() {
   return (
     <div className="App">      
       <Navbar />
-      <Home />
+      <SlideShow />
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,80 +1,121 @@
+import React, { useState, useRef } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import conComemoracao from "../assets/img/news_enfermagem_condado.jpg";
+import vttTanqueOxigenio from "../assets/img/news_tanque.png";
+import tbbAmbulatorio from "../assets/img/news_inauguracao_ambulatorio_tbb.jpg";
+import sjeCampanha from "../assets/img/news_sje.jpg";
+import allCampanhaVacinacao from "../assets/img/news_vacinacao.jpg";
+import allConsumoAgua from "../assets/img/news_agua.jpg";
+import allTransparenciaPortal from "../assets/img/news_portal.png";
+import allRedesSociais from "../assets/img/news_agua.jpg";
+import allSaudavel from "../assets/img/news_frutas.jpg";
 import "./App.css";
-import React from "react";
 
-export const Home = () => {
-    return (
-      <div className="body-content">
-        <div className="container_slide">
-          <div className="content-left">
-            <div className="slider">
-              <div className="slider-control">
-                <span id="prev">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="48"
-                    height="48"
-                    viewBox="0 0 24 24"
-                    style={{ fill: "rgba(255, 255, 255, 1)" }}
-                  >
-                    <path d="M13.293 6.293 7.586 12l5.707 5.707 1.414-1.414L10.414 12l4.293-4.293z"></path>
-                  </svg>{" "}
-                </span>
-                <span id="next">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="48"
-                    height="48"
-                    viewBox="0 0 24 24"
-                    style={{ fill: "rgba(255, 255, 255, 1)" }}
-                  >
-                    <path d="M10.707 17.707 16.414 12l-5.707-5.707-1.414 1.414L13.586 12l-4.293 4.293z">
-                    </path>
-                  </svg>
-                </span>
+const Home = () => {
+  const [newsData] = useState([
+    {
+      image: conComemoracao,
+      title: "Condado – Comemoração na área de saúde",
+      link: "news/news_enfermagem_condado.html",
+    },
+    {
+      image: vttTanqueOxigenio,
+      title:
+        "Vertentes - Novo tanque de oxigênio no Hospital Evaristo Ferreira Filho",
+      link: "news/news_tanque.html",
+    },
+    {
+      image: tbbAmbulatorio,
+      title:
+        "Timbaúba - Inauguração do Ambulatório Dr. Milton Cândido da Silva",
+      link: "news/news_ambulatorio_tbb.html",
+    },
+    {
+      image: sjeCampanha,
+      title: "São José do Egito - Campanha Interiorizar a Saúde",
+      link: "news/news_sje.html",
+    },
+    {
+      image: allCampanhaVacinacao,
+      title: "Campanha de Vacinação 2023",
+      link: "news/news_vacinacao.html",
+    },
+    {
+      image: allConsumoAgua,
+      title: "Consumo de Água",
+      link: "news/news_agua.html",
+    },
+    {
+      image: allTransparenciaPortal,
+      title: "CHS divulga área de transparência no Portal",
+      link: "news/news_portal.html",
+    },
+    {
+      image: allRedesSociais,
+      title: "CHS divulga suas redes sociais",
+      link: "news/news_redessociais.html",
+    },
+    {
+      image: allSaudavel,
+      title: "Saiba o que fazer para manter sua vida saudável",
+      link: "news/news_saudavel.html",
+    },
+  ]);
+
+  const sliderRef = useRef(null);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+  };
+
+  return (
+    <div className="body-content">
+      <div className="slider-container">
+        <div className="content-left">
+          <Slider {...settings} ref={sliderRef}>
+            {newsData.map((news, index) => (
+              <div key={index}>
+                <img src={news.image} alt={news.title} />
+                <div className="cycle-overlay">
+                  <a href={news.link}>{news.title}</a>
+                </div>
               </div>
-  
-              <div
-                className="cycle-slideshow"
-                data-cycle-fx="fade"
-                data-cycle-slides="> div"
-                data-cycle-timeout="4000"
-              >
-                <div>
-                  <img src="/img/news_enfermagem_condado.jpg" alt="Condado – Comemoração na área de saúde" />
-                  <div className="cycle-overlay">
-                    <a href="news/news_enfermagem_condado.html">Condado – Comemoração na área de saúde</a>
-                  </div>
-                </div>
-                <div>
-                  <img src="/img/news_tanque.png" alt="Vertentes - Novo tanque de oxigênio no Hospital Evaristo Ferreira Filho" />
-                  <div className="cycle-overlay">
-                    <a href="news/news_tanque.html">Vertentes - Novo tanque de oxigênio no Hospital Evaristo Ferreira Filho</a>
-                  </div>
-                </div>
-                {/* Adicione mais slides aqui */}
-              </div>
-            </div>
-          </div>
-          <div className="content-right">
-            <div className="news-board">
-              <h3>Últimas Notícias</h3>
-              <div className="news-roller">
-                <div className="slot1">
-                  <h2>Condado – Comemoração na área de saúde</h2>
-                  <a href="news/news_enfermagem_condado.html" className="link">Saiba Mais</a>
-                </div>
-                <div className="slot1">
-                  <h2>Vertentes - Novo tanque de oxigênio no Hospital Evaristo Ferreira Filho</h2>
-                  <a href="news/news_tanque.html" className="link">Saiba Mais</a>
-                </div>
-                {/* Adicione mais notícias aqui */}
-              </div>
-              <a className="slot2handle" href="Noticias.html" style={{ textAlign: "center" }}>Ver Todas</a>
-            </div>
-          </div>
+            ))}
+          </Slider>
         </div>
       </div>
-    );
-  };
-  
-  export default Home;
+      <div className="content-right">
+        <div className="news-board">
+          <h3>Últimas Notícias</h3>
+          <div className="news-roller">
+            {newsData.map((news, index) => (
+              <div className="slot1" key={index}>
+                <h2>{news.title}</h2>
+                <a href={news.link} className="link">
+                  Saiba Mais
+                </a>
+              </div>
+            ))}
+          </div>
+          <a
+            className="slot2handle"
+            href="Noticias.html"
+            style={{ textAlign: "center" }}
+          >
+            Ver Todas
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
